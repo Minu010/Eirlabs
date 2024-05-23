1bf000061cb5fe3a173c83b6836033af95c53ab0
Minu010 <minumassengo@alumnes.ilerna.com>
2024-05-23 15:47:27 +0200

    eirlabs 2

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { AyudaComponent } from './ayuda/ayuda.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'sobre-nosotros', component: AboutUsComponent },
+  { path: 'ayuda', component: AyudaComponent},
 
   // Otras rutas aquí si las tienes
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige al componente de inicio de sesión por defecto
--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+
+  mostrarNavbarFooter: boolean = true;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const currentRoute = this.activatedRoute.firstChild?.snapshot.routeConfig?.path;
+      this.mostrarNavbarFooter = !(currentRoute === 'login' || currentRoute === 'register');
+    });
+  }
 }
